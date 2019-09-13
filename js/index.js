21019428a4d0b5c2510f4a4a0fb157ef84d6dcc1
biggestbarnacle1 <52912384+biggestbarnacle1@users.noreply.github.com>
2019-09-12 22:36:55 -0600

    Update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const background_image = document.querySelector("#showcase");
 const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 const timeline_1 = new TimelineMax();
 
-timeline_1.fromTo(background_image,1, {height: "0px"}, {height: "452px"}).fromTo(header, 1, {width: "60%"}, {width: "100%"}, "-=1");
+timeline_1.fromTo(background_image,2, {height: "0px"}, {height: "452px"}).fromTo(header, 3, {width: "60%"}, {width: "100%"}, "-=2");
